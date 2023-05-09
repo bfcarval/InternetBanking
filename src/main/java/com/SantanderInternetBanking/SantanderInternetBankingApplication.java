6d0abe0bb7decf3c65779ced0e97163e16895c39
Bruno Carvalho <brunocarval.fernandes@gmail.com>
2023-05-09 14:10:52 -0300

    [Master] First commit.

---
+++ src/main/java/com/SantanderInternetBanking/SantanderInternetBankingApplication.java
@@ -0,0 +1,13 @@
+package com.SantanderInternetBanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderInternetBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderInternetBankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SantanderInternetBanking/SantanderInternetBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SantanderInternetBanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderInternetBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
